9bcbdac1db7a779d197757350f0b831f14a13f6c
joao.grilo <joao.grilo@meta.com.br>
2022-08-22 11:53:12 -0300

    implementing the css modules

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import './style.scss';
+import style from './button.module.scss';
 class Button extends React.Component{
     render() {
         return (
-            <button className="button">
+            <button className={style.button}>
                 Botão
             </button>
         )
