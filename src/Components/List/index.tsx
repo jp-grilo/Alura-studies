be9ecac38656f258704f90c4c11e2739a826675d
joao.grilo <joao.grilo@meta.com.br>
2022-08-30 11:18:17 -0300

    iniciado o processo de dinamização da lista

--- src/Components/List/index.tsx
+++ src/Components/List/index.tsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from './Item'
 import style from './list.module.scss';
 
 function List(){
-    const tarefas = [{
-        tarefa: 'React',
-        tempo: '02:00:00'
-    }, {
-        tarefa: 'JS',
-        tempo: '01:00:00'
-    }, {
-        tarefa: 'TS',
-        tempo: '03:00:00'
-    }]
+    const [tarefas, setTarefas] = useState(
+        [{
+            tarefa: 'React',
+            tempo: '02:00:00'
+        }, {
+            tarefa: 'JS',
+            tempo: '01:00:00'
+        }, {
+            tarefa: 'TS',
+            tempo: '03:00:00'
+        }]
+    )
 
     return(
         <aside className={style.taskList}>
-            <h2> Estudos do dia</h2>
+            <h2 onClick={() => {
+                setTarefas([...tarefas, {tarefa: "Estudar estado", tempo: "05:00:00"}]);
+            }}
+            
+            > Estudos do dia</h2>
             <ul >
                 {tarefas.map((item, index) => (
                     <Item 
