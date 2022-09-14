8747c96683e64bd163d7868b524c81705100fead
jp-grilo <joaoplopescg@gmail.com>
2022-09-14 09:42:58 -0300

    estilização do card selecionado

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -16,7 +16,7 @@ export default function Item(
   }: Props) {
   return (
     <li 
-    className={style.item} 
+    className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
     onClick={() => selecionaTarefa(
       {
         tarefa,
