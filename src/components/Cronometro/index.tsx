7a56aec8975835677a7cf046b0599111cd309040
jp-grilo <joaoplopescg@gmail.com>
2022-09-14 10:30:18 -0300

    comunicação cronometro com item selecionado

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -2,13 +2,23 @@ import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss';
 import tempoParaSegundos from "../../common/utils/date";
+import { useState } from "react";
+import { ITarefa } from "../../types/tarefa";
 
 
-export default function Cronometro() {
+interface Props{
+  selecionado: ITarefa | undefined
+}
 
+export default function Cronometro({ selecionado }: Props) {
+  const [tempo, setTempo] = useState<number>();
+    if(selecionado?.tempo) {
+        setTempo(tempoParaSegundos(selecionado.tempo));
+    }
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
+      tempo:{tempo}
       <div className={style.relogioWrapper}>
         <Relogio />
       </div>
