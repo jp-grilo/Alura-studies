412cb081ce3e8701d56b4e0947c2d251fa9b4b7c
jp-grilo <joaoplopescg@gmail.com>
2022-09-14 08:36:44 -0300

    uuid added

--- src/Components/List/Item/index.tsx
+++ src/Components/List/Item/index.tsx
@@ -1,5 +1,6 @@
+import ITarefa from '../../../types/tarefas';
 import style from '../list.module.scss';
-export default function Item({tarefa, tempo} : { tarefa: string, tempo: string}){
+export default function Item({tarefa, tempo, selecionado, completado, id} : ITarefa){
         return(
             <li className={style.item}>
                 <h3>{tarefa}</h3>
