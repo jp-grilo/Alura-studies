b299183aee024393bd313a3405904bb98a9bb19d
jp-grilo <joaoplopescg@gmail.com>
2022-09-14 10:01:44 -0300

    função de conversaão de tempo para segundos

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,8 +1,11 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss';
+import tempoParaSegundos from "../../common/utils/date";
+
 
 export default function Cronometro() {
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
