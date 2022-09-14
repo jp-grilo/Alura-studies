51f4c825c2beb1b0981401bea2a17eb8b495b166
jp-grilo <joaoplopescg@gmail.com>
2022-09-14 10:43:32 -0300

    pegando mudança ed tempo dos cards

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -2,7 +2,7 @@ import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss';
 import tempoParaSegundos from "../../common/utils/date";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ITarefa } from "../../types/tarefa";
 
 
@@ -12,9 +12,12 @@ interface Props{
 
 export default function Cronometro({ selecionado }: Props) {
   const [tempo, setTempo] = useState<number>();
+  useEffect(() => {
     if(selecionado?.tempo) {
-        setTempo(tempoParaSegundos(selecionado.tempo));
+     setTempo(tempoParaSegundos(selecionado.tempo));
     }
+  }, [selecionado])
+  
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
